367d0e20fc56ad01b2c8b839c84ad43185604af7
botsarefuture <vuoreol@pm.me>
2025-11-30 14:20:35 +0200

    fixed spammy logs issue

--- AutoSec/catguard.py
+++ AutoSec/catguard.py
@@ -12,7 +12,7 @@ ALERT_LEVEL_SELF_DESTRUCT = 6
 
 # ----------------- Logging -----------------
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.WARNING,
     format="%(asctime)s - %(levelname)s - %(message)s"
 )
 logger = logging.getLogger("watchthecat")
